94bebb936c91ee98eb69bfb0128ea8250ef6f177
dodo721 <dodoworkspr@gmail.com>
2022-05-08 22:23:25 +0100

    commands

---
+++ deploy-commands.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, token } = require('./config.json');
+
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Check server is alive!'),
+	new SlashCommandBuilder().setName('status').setDescription('Gives stat rundown of the server'),
+    new SlashCommandBuilder().setName('restart').setDescription('Restart the server (takes a while!)'),
+    new SlashCommandBuilder().setName('stop').setDescription('Stops the server'),
+    new SlashCommandBuilder().setName('piss').setDescription('piss'),
+].map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
--- index.js
+++ index.js
@@ -1,12 +1,52 @@
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
+const { exec, spawn } = require('child_process');
 
 if (!token) throw new Error("Missing token from config.json!");
 
+// Server process
+const mcServerProc = spawn('cd ~/minecraft/ && ./run.sh');
+
+const onLog = data => {
+    process.stdout.write(data.toString());
+}
+
+mcServerProc.stdout.on('data', onLog);
+mcServerProc.stderr.on('data', onLog);
+
+const mcCommand = async command => {
+    mcServerProc.stdin.write(command+'\n');
+
+    // buffer output for a quarter of a second, then reply to HTTP request
+    var buffer = [];
+    var collector = function(data) {
+        data = data.toString();
+        buffer.push(data.split(']: ')[1]);
+    };
+    mcServerProc.stdout.on('data', collector);
+    await new Promise(r => setTimeout(r, 250));
+    mcServerProc.stdout.removeListener('data', collector);
+    return buffer.join('');
+}
+
+// Discord bot
 const client = new Client({intents: [Intents.FLAGS.GUILDS]});
 
 client.once('ready', () => {
     console.log("Logged in!");
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName == 'stop') {
+        const response = await mcCommand('stop');
+        await interaction.reply(response);
+    }
 });
 
 client.login(token);
