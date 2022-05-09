53adf0650f7965b62eb768f850555b7a4c225304
dodo721 <dodoworkspr@gmail.com>
2022-05-09 17:25:43 +0100

    corrected memory command

--- pcStats.js
+++ pcStats.js
@@ -1,7 +1,7 @@
 const { execSync, exec } = require("child_process");
 
 const percentMemUsed = () => {
-    const memory = execSync("free | grep Mem | awk '{ printf(\"free: %.4f %\n\", $4/$2 * 100.0) }'")
+    const memory = execSync("free | grep Mem | awk '{print $3/$2 * 100.0}'")
     return memory;
 }
 
