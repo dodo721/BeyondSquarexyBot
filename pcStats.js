d99d299446a2533f0ae47f87d7cbf430380a5342
dodo721 <dodoworkspr@gmail.com>
2022-05-09 17:26:31 +0100

    removed newline

--- pcStats.js
+++ pcStats.js
@@ -2,7 +2,7 @@ const { execSync, exec } = require("child_process");
 
 const percentMemUsed = () => {
     const memory = execSync("free | grep Mem | awk '{print $3/$2 * 100.0}'")
-    return memory;
+    return memory.toString().replace(/\n$/g, "");
 }
 
 module.exports = {percentMemUsed};
